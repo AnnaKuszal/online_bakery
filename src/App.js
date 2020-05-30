169ffea25303c98e1fae6cb75ca17be3030fd0f0
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-30 21:38:31 +0200

    Modify App.js add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 
-export default App;
+import './styles/global.scss';
+
+import { store } from './redux/store';
+
+import { MainLayout } from './components/layout/MainLayout/MainLayout';
+import { Homepage } from './components/views/Homepage/Homepage';
+import { ProductsList } from './components/views/ProductsList/ProductsList';
+import { Product } from './components/views/Product/Product';
+import { Cart } from './components/views/Cart/Cart';
+import { NotFound } from './components/views/NotFound/NotFound';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#634206' },
+  },
+});
+
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <MainLayout>
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route exact path='/productsList' component={ProductsList} />
+              <Route exact path='/product/:id' component={Product} />
+              <Route exact path='/cart' component={Cart} />
+              <Route path='*' component={NotFound} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
+    </BrowserRouter>
+  </Provider>
+);
+
+export { App };
